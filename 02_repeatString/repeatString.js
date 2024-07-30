4a24ebc31d5ebff921b7d80b8e8f82304a612954
Philip-11 <magsaysayphil@gmail.com>
2024-07-30 16:41:25 +0800

    Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    let returnedString = "";
+    for (i = 0; i < num; i++){
+        returnedString = returnedString + string;
+    }
 
+    return returnedString;
 };
-
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
